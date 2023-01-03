451a4108894db39a6ee3dff696a207e6194a246f
Senu14 <101559338+Senu14@users.noreply.github.com>
2023-01-03 12:35:00 +0100

    dfgfdh

---
+++ views/new.js
@@ -0,0 +1,18 @@
+const ArrContent = [ 
+     {title:'products', content: 'lorem ipsul'},
+     {title:'Om os', content: 'lorem ipsul'},
+     {title:'Login', content: 'lorem ipsul'}
+    ]
+    
+    function Link(pageID) {
+      console.log(ArrContent[pageID]);
+    
+    }
+    
+    
+    // import Navbar from "./navbar"
+    
+    // function App() {
+    //   return <Navbar />
+    // }
+    // export default App
